52bd44b42a83e433d9a6f31c8d06a00e8160a857
Lucas Camillo <lucas_camillo@brown.edu>
2018-09-25 09:34:48 -0400

    Up to problem 2

--- Week 03/pre-class-03.R
+++ Week 03/pre-class-03.R
@@ -6,3 +6,27 @@ install.packages("gapminder")
 library(dplyr)
 library(gapminder)
 gapminder = gapminder # dataset is called gapminder
+
+#1)
+#52 in Africa, 25 in Americas, 33 in Asia, 30 in Europe, 2 in Oceania
+gapminder %>%
+  group_by(continent) %>%
+  distinct(country) %>%
+  count()
+
+#2)
+# It is Albania with 3193 for gdpPercap
+gapminder %>%
+  filter(continent == "Europe", year == "1997") %>%
+  arrange(gdpPercap)
+
+#3)
+
+
+
+
+
+
+
+
+
