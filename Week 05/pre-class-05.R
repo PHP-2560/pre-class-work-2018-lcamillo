5acedd491bba1daf119ff0f3299d8772f9ea0c74
Lucas Camillo <lucas_camillo@brown.edu>
2018-10-05 11:48:07 -0400

    Completed

--- Week 05/pre-class-05.R
+++ Week 05/pre-class-05.R
@@ -38,17 +38,44 @@ wcgs.new
 #d-
 #they should have a mean of 0 and a sd of 1. The sds are 1 and the means are very close to 0 due to tiny errors in the assignment of the numbers.
 for (i in 2:ncol(wcgs.new)) {
-  print(paste('Mean of ', names(wcgs.new)[i], 'is ', mean(wcgs.new[,i], na.rm = TRUE)))
-  print(paste('Sd of ', names(wcgs.new)[i], 'is ', sd(wcgs.new[,i], na.rm = TRUE)))
+  print(paste('Mean of', names(wcgs.new)[i], 'is', mean(wcgs.new[,i], na.rm = TRUE)))
+  print(paste('Sd of', names(wcgs.new)[i], 'is', sd(wcgs.new[,i], na.rm = TRUE)))
 }
 
 
 
 #2)
 
+#a-
+no_CHD = vector()
+MI_or_SD = vector()
+silent_MI = vector()
+angina = vector()
+for (i in 1:nrow(wcgs)) {
+  if (wcgs[i, 12] == "no CHD") {
+    no_CHD = append(no_CHD, wcgs[i, 4])
+  } else if (wcgs[i, 12] == "MI or SD") {
+    MI_or_SD = append(MI_or_SD, wcgs[i, 4])
+  } else if (wcgs[i, 12] == "silent MI") {
+    silent_MI = append(silent_MI, wcgs[i, 4])
+  } else if (wcgs[i, 12] == "angina") {
+    angina = append(angina, wcgs[i, 4])
+  }
+}
+
+mean(unlist(no_CHD), na.rm = TRUE)
+mean(unlist(MI_or_SD), na.rm = TRUE)
+mean(unlist(silent_MI), na.rm = TRUE)
+mean(unlist(angina), na.rm = TRUE)
 
 
+#b-
+library(dplyr)
+wcgs = as_tibble(wcgs)
 
+wcgs %>%
+  group_by(typchd69) %>%
+  summarise(avg_weight = mean(weight))
 
 
 
