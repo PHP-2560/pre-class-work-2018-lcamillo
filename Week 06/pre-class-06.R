01b5048a9d53c26f7a8907d69d61547b097dfd4b
Lucas Camillo <lucas_camillo@brown.edu>
2018-10-16 11:02:55 -0400

    Up to 1 completed

--- Week 06/pre-class-06.R
+++ Week 06/pre-class-06.R
@@ -2,3 +2,19 @@
 #B01478147
 #Lucas Paulo de Lima Camillo
 
+#1)
+#checks if a substring is a prefix of a string
+is_prefix <- function(string, prefix) {
+  substr(string, 1, nchar(prefix)) == prefix
+}
+
+#removes the last element of a list or vector
+remove_last <- function(x) {
+  if (length(x) <= 1) return(NULL)
+  x[-length(x)]
+}
+
+#repeats y for the length of x
+rep_length <- function(x, y) {
+  rep(y, length.out = length(x))
+}
