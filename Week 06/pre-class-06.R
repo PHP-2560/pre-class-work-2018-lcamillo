b15a575e30726669351831ff39cf798b44a4304a
Lucas Camillo <lucas_camillo@brown.edu>
2018-10-16 11:22:55 -0400

    Up to problem 2

--- Week 06/pre-class-06.R
+++ Week 06/pre-class-06.R
@@ -2,6 +2,7 @@
 #B01478147
 #Lucas Paulo de Lima Camillo
 
+
 #1)
 #checks if a substring is a prefix of a string
 is_prefix <- function(string, prefix) {
@@ -18,3 +19,17 @@ remove_last <- function(x) {
 rep_length <- function(x, y) {
   rep(y, length.out = length(x))
 }
+
+
+#2)
+#rnorm is just a simpler version of MASS::mvrnorm(). For instance, when mu = 0, Sigma = 1, empirical = FALSE, they are basically the same.
+#However, MASS::mvrnorm() is more powerful, since it can be used with multiple variables instead of just two. Also, it outputs a matrix instead of the vector for rnorm().
+#In order to make them more consistent, they could both have the same type of output. Also, MASS::mvrnorm() could have mu = 0 and sigma = 1 as standard, since that would be the standard normal distribution.
+
+
+
+
+
+
+
+
